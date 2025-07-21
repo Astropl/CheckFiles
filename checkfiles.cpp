634dfb07a139aa28169b013653795f0eb4c46236
Paweł Martys <pmartys@acm.lan>
2025-07-21 13:34:25 +0200

    drugi

--- checkfiles.cpp
+++ checkfiles.cpp
@@ -1,7 +1,11 @@
 #include "checkfiles.h"
 #include "./ui_checkfiles.h"
 #include <QCoreApplication>
-
+//#include "mainwindow.h"
+//#include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QFileInfo>
+#include <QFile>
 
 
 
@@ -10,9 +14,60 @@ CheckFiles::CheckFiles(QWidget *parent)
     , ui(new Ui::CheckFiles)
 {
     ui->setupUi(this);
+
+    connect(ui->btnChooseSource, &QPushButton::clicked, this, &CheckFiles::chooseSourceDir);
+    connect(ui->btnChooseTarget, &QPushButton::clicked, this, &CheckFiles::chooseTargetDir);
+    connect(ui->btnStart, &QPushButton::clicked, this, &CheckFiles::startMonitoring);
+
+
 }
 
 CheckFiles::~CheckFiles()
 {
     delete ui;
+}
+
+void CheckFiles::chooseSourceDir() {
+    sourceDir = QFileDialog::getExistingDirectory(this, "Wybierz folder źródłowy");
+    ui->lineSource->setText(sourceDir);
+}
+
+void CheckFiles::chooseTargetDir() {
+    targetDir = QFileDialog::getExistingDirectory(this, "Wybierz folder docelowy");
+    ui->lineTarget->setText(targetDir);
+}
+
+void CheckFiles::startMonitoring() {
+    if (sourceDir.isEmpty() || targetDir.isEmpty()) {
+        log("Wybierz oba foldery najpierw!");
+        return;
+    }
+
+    watcher.addPath(sourceDir);
+    connect(&watcher, &QFileSystemWatcher::directoryChanged, this, &CheckFiles::onDirectoryChanged);
+    log("Monitoring rozpoczęty...");
+}
+
+void CheckFiles::onDirectoryChanged(const QString& path) {
+    QDir dir(sourceDir);
+    QFileInfoList files = dir.entryInfoList(QDir::Files);
+    for (const QFileInfo& file : files)
+        syncFile(file.filePath());
+}
+
+void CheckFiles::syncFile(const QString& path) {
+    QFileInfo fi(path);
+    QString destPath = targetDir + "/" + fi.fileName();
+    if (QFile::exists(path)) {
+        QFile::remove(destPath);
+        if (QFile::copy(path, destPath)) {
+            log("Skopiowano: " + fi.fileName());
+        } else {
+            log("Błąd kopiowania: " + fi.fileName());
+        }
+    }
+}
+
+void CheckFiles::log(const QString& message) {
+    ui->logBox->appendPlainText(QTime::currentTime().toString() + " - " + message);
 }
--- checkfiles.h
+++ checkfiles.h
@@ -1,6 +1,8 @@
 #ifndef CHECKFILES_H
 #define CHECKFILES_H
-
+#include <QFileSystemWatcher>
+#include <QString>
+#include <QDir>
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -16,6 +18,22 @@ class CheckFiles : public QMainWindow
 public:
     CheckFiles(QWidget *parent = nullptr);
     ~CheckFiles();
+
+private slots:
+    void chooseSourceDir();
+    void chooseTargetDir();
+    void startMonitoring();
+    void onDirectoryChanged(const QString& path);
+
+private:
+    void syncFile(const QString& path);
+    void log(const QString& message);
+
+    //Ui::MainWindow *ui;
+    QFileSystemWatcher watcher;
+    QString sourceDir;
+    QString targetDir;
+
 
 private:
     Ui::CheckFiles *ui;
